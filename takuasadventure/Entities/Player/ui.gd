c282347f5843bd405010165837a3381ee3d59f55
MesaEhren <mesa.ehren@gmail.com>
2025-07-02 16:25:04 -0500

    Basic UI healthbar.
    Assets and super, super basic code for the health bar UI.

---
+++ takuasadventure/Entities/Player/ui.gd
@@ -0,0 +1,8 @@
+extends Control
+
+#TEST CODE DELETE THIS LATER PLS
+func _input(_event: InputEvent) -> void:
+	if Input.is_key_label_pressed(KEY_O):
+		$TextureProgressBar.value += 1
+	if Input.is_key_label_pressed(KEY_P):
+		$TextureProgressBar.value -= 1
